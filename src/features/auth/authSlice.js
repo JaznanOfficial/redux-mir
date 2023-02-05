5404f496fc7ce635457334d7424262c3287d9610
JaznanOfficial <jaznanofficial@gmail.com>
2023-02-05 18:31:23 +0600

    firebase auth done

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+
+const initialState = {
+    email: "",
+    role: "",
+    isLoading: true,
+    isError: false,
+    error: "",
+};
+
+export const createUser = createAsyncThunk("auth/createUser", async ({ email, password }) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+});
+
+export const loginUser = createAsyncThunk("auth/loginUser", async ({ email, password }) => {
+    const data = await signInWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+});
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        logOut: (state) => {
+            state.email = "";
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(createUser.pending, (state) => {
+            state.isLoading = true;
+            state.isError = false;
+            state.error = "";
+        });
+        builder.addCase(createUser.fulfilled, (state, { payload }) => {
+            state.isLoading = false;
+            state.email = payload;
+            state.isError = false;
+            state.error = "";
+        });
+        builder.addCase(createUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.email = "";
+            state.isError = true;
+            state.error = action.error.message;
+        });
+        builder.addCase(loginUser.pending, (state) => {
+            state.isLoading = true;
+            state.isError = false;
+            state.error = "";
+        });
+        builder.addCase(loginUser.fulfilled, (state, { payload }) => {
+            state.isLoading = false;
+            state.email = payload;
+            state.isError = false;
+            state.error = "";
+        });
+        builder.addCase(loginUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.email = "";
+            state.isError = true;
+            state.error = action.error.message;
+        });
+    },
+});
+
+export const { logOut } = authSlice.actions;
+export default authSlice.reducer;
